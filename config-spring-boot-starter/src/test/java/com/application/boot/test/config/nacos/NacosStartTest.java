a6312e9cd9cdf2ecdb929b1f72dd43cc240e9385
luckysoftgo <supingemail@126.com>
2019-12-04 14:32:19 +0800

    提交配置項目...

---
+++ config-spring-boot-starter/src/test/java/com/application/boot/test/config/nacos/NacosStartTest.java
@@ -0,0 +1,12 @@
+package com.application.boot.test.config.nacos;
+
+import com.application.boot.test.BasicStartTest;
+
+/**
+ * @author : 孤狼
+ * @NAME: NacosStartTest
+ * @DESC:
+ **/
+public class NacosStartTest extends BasicStartTest {
+
+}
